d07bc5eb1c98bac5bf416f7f75dc1e68ec34ceba
Caimee <witty8010@outlook.com>
2026-06-23 13:56:07 +0800

    update avatar

--- src/config.ts
+++ src/config.ts
@@ -69,7 +69,7 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	avatar: "assets/images/demo-avatar.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "Modo",
 	bio: "Audentes fortuna iuvat!",
 	links: [
